ad3f3e89cd5998cf4108a0a2ff43402473ea4640
Rongjian Zhang <pd4d10@gmail.com>
2022-06-12 12:29:51 +0800

    refactor: lazy load module

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,36 @@
 import fs from 'fs-extra'
-import { runTsc, buildJs, bundleDts } from './build.js'
 import { InlineConfig, resolveConfig } from './config.js'
-import { runTest } from './test.js'
 
 export async function watch(config: InlineConfig) {
   const resolved = await resolveConfig(config)
+  const { runTsc, buildJs } = await import('./build.js')
 
   await fs.emptyDir(resolved.outDir)
   await Promise.all([
-    buildJs(resolved, true, 'es'),
-    buildJs(resolved, true, 'cjs'),
-    buildJs(resolved, true, 'umd'),
+    ...resolved.formats.map((format) => buildJs(resolved, true, format)),
     resolved.tsc !== false ? runTsc(resolved, true) : Promise.resolve(),
   ])
 }
 
 export async function build(config: InlineConfig) {
   const resolved = await resolveConfig(config)
+  const { runTsc, buildJs, bundleDts } = await import('./build.js')
 
   if (resolved.tsc !== false) await runTsc(resolved)
+
   const { writeDts } = await bundleDts(resolved)
   await fs.emptyDir(resolved.outDir)
 
   for (let format of resolved.formats) {
     await buildJs(resolved, false, format)
   }
-
   await writeDts()
 }
 
 export async function test(config: InlineConfig) {
   const resolved = await resolveConfig(config)
 
+  const { runTest } = await import('./test.js')
   await runTest(resolved)
 }
 
